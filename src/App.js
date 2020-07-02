ccb5a56163d2506ee1f5e82c160f9f2d372ad83e
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-07-01 22:14:59 -0300

    feature: ajustando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Layout from '@vtex/styleguide/lib/Layout'
+import Card from '@vtex/styleguide/lib/Card'
+
+import Collapsible from '@vtex/styleguide/lib/Collapsible'
 
 function App() {
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout fullWidth>
+      <Card>
+        <Collapsible
+          isOpen={isOpen}
+          onClick={() => { setIsOpen(!isOpen) }}
+          header="Carai"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          oi
+        </Collapsible>
+      </Card>
+    </Layout>
   );
 }
 
